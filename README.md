49e5882a40a234c056471c7bd2b790c59f23ea06
sabbirahm3d <sabbir0ahmed0@gmail.com>
2019-04-27 14:34:48 -0400

    figured I should add some docs

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Sortify
 
 Stupid simple script I wrote in an hour to sort my Spotify playlists the way I want it. Also that's a cool name
+
+## Usage
+Install the requirements.
+```pip install -r requirements.txt```
+
+Set up an app on your Spotify dev account. Save the Client ID and secret from there, and add a redirect URI in your whitelist.
+
+Oh, you thought just signing into Spotify would do that for you? Too bad, I can't be bothered atm it's raining outside and I'm watching reruns of Law and Order's season 2. They really should've stopped after season 6, I think they're on 17 now and aiming to outdo the Simpsons. They had Logan Paul in an episode with gamers lolwtf.
+
+Anyways, set up a `secret.py` and put the environment variables in:
+```python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+CLIENT_ID = "BLABLABALBALBALBALBALblablablaBL"
+CLIENT_SECRET = "HUNTER2HUNTER2HUNTER2HUNTER2HUNT"
+REDIRECT_URI = "http://localhost:8000/billie_eilish_may_be_an_industry_plant_for_teen_girls_but_shes_way_better_than_cardi_b"
+```
+
+Run `sortipy.py`. Of course it's in Python 3, get with the times boomer.
+
+You'll get prompted for the redirect URI which you can copypasta from your browser you got redirected to. Bada bing bada boom you're done, you can now sort your Spotify playlist by time signatures, BPM and the length of the track names for all I care. :)
--- sortify.py
+++ sortify.py
@@ -12,36 +12,53 @@ from secret import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 class Sortify(object):
 
     def __init__(self, client_id, client_secret, redirect_uri, user_id):
+        """read readme pls
 
+        Arguments:
+            client_id {str} -- Client ID for Spotify app
+            client_secret {str} -- Client secret for Spotify app
+            redirect_uri {str} -- Redirect URI for Spotify app
+            user_id {str} -- Your user ID
+        """
         self.user_id = user_id
 
         os.environ["SPOTIPY_CLIENT_ID"] = CLIENT_ID
         os.environ["SPOTIPY_CLIENT_SECRET"] = CLIENT_SECRET
         os.environ["SPOTIPY_REDIRECT_URI"] = REDIRECT_URI
 
-        scope = "playlist-modify-public"
-        token = util.prompt_for_user_token(self.user_id, scope)
-
-        self.sp = Spotify(auth=token)
+        self.sp = Spotify(
+            auth=util.prompt_for_user_token(
+                self.user_id,
+                "playlist-modify-public"
+            )
+        )
         self.sp.trace = False
 
     def sort_by_attributes(self, playlist, attrs):
+        """Recursively sorts the playlist one attribute at a time
 
+        Arguments:
+            playlist {list} -- Playlist response object
+            attrs {list} -- List of attributes to sort by. List must be
+                            mutable.
+
+        Returns:
+            [list] -- Sorted playlist - who could've guessed?
+        """
         if len(attrs):
 
             attr = attrs.pop()
 
             if attr == "artists":
-                playlist.sort(key=lambda x: x["track"]
-                              ["artists"][0]["name"].lower())
+                playlist.sort(key=lambda x: x["artists"][0]["name"].lower())
                 return self.sort_by_attributes(playlist, attrs)
 
             elif attr == "tracks":
-                playlist.sort(key=lambda x: x["track"]["name"].lower())
+                playlist.sort(key=lambda x: x["name"].lower())
                 return self.sort_by_attributes(playlist, attrs)
 
             elif attr == "album_release_date":
-                playlist.sort(key=lambda x: x["track"]["album"]["release_date"])
+                playlist.sort(key=lambda x: x["album"]["release_date"])
                 return self.sort_by_attributes(playlist, attrs)
 
             else:
@@ -49,10 +66,26 @@ class Sortify(object):
 
         return playlist
 
-    def sort_playlist(self, playlist_name, sort_by, show=False):
+    def sort_playlist(self, playlist_name, sort_by, ready, custom=None,
+                      show=False):
+        """Sorts playlist lol
 
+        Arguments:
+            playlist_name {str} -- Name of the playlist duh. Will raise an
+                                   exception if the playlist isn't associated
+                                   with the username. Which is also a good
+                                   indication the user does not own the
+                                   playlist.
+            sort_by {list} -- List of attributes to sort by. List must be
+                              mutable.
+            ready {bool} -- Option to commit changes to the updated playlist.
+            custom {func} -- Optional custom sorting key, like if you wanna
+                             sort by track name sring length or something.
+            show {bool} (default: False) -- Option to dump the reordered
+                                              playlist in a purdy format.
+        """
         playlists = self.sp.user_playlists(self.user_id)
-        sorted_playlist = []
+        new_playlist = []
 
         for playlist in playlists["items"]:
             if playlist["name"] == playlist_name:
@@ -60,21 +93,23 @@ class Sortify(object):
                     self.user_id, playlist["id"], fields="tracks,next"
                 )
                 tracks = results["tracks"]
-                sorted_playlist.extend(tracks["items"])
+                new_playlist.extend([i["track"] for i in tracks["items"]])
 
                 while tracks["next"]:
                     tracks = self.sp.next(tracks)
-                    sorted_playlist.extend(tracks["items"])
+                    new_playlist.extend([i["track"] for i in tracks["items"]])
 
-                sorted_playlist = self.sort_by_attributes(
-                    sorted_playlist, sort_by)
+                new_playlist = self.sort_by_attributes(new_playlist, sort_by)
+
+                if custom:
+                    new_playlist.sort(key=custom)
 
                 if show:
                     print("Playlist name: {}".format(playlist_name))
                     print("Playlist ID: {}\n".format(playlist["id"]))
                     print("   {:3s} {:60s} {:s}".format("", "Track", "Artist"))
-                    for i, item in enumerate(sorted_playlist):
-                        track = item["track"]
+                    for i, item in enumerate(new_playlist):
+                        track = item
                         print(
                             "   {:3d} {:60s} {:s}".format(
                                 i + 1,
@@ -83,23 +118,29 @@ class Sortify(object):
                             )
                         )
 
-                self.sp.user_playlist_replace_tracks(
-                    self.user_id,
-                    playlist["id"],
-                    [x["track"]["uri"] for x in sorted_playlist]
-                )
-                print("Playlist successfully updated")
+                if ready:
+                    self.sp.user_playlist_replace_tracks(
+                        self.user_id,
+                        playlist["id"],
+                        [x["uri"] for x in new_playlist]
+                    )
+                    print("Playlist successfully updated")
                 break
 
-        if not len(sorted_playlist):
+        if not len(new_playlist):
             raise AttributeError("Playlist not found")
 
 
 if __name__ == "__main__":
 
     obj = Sortify(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, "12164367064")
+
+    # sort by album release date, artist name, track name and of course the
+    # third character of each track name lol
     obj.sort_playlist(
         "MMXIX",
         sort_by=["album_release_date", "artists", "tracks"],
+        custom=lambda x: x["name"][2],
+        ready=False,
         show=True
     )
